e31e5d2a53a6a94c86da349413ce7891f4fd4c75
Nitid Comvandee <ncomvandee@seattleu.edu>
2021-01-27 18:53:36 -0800

    Refactored CellReadPage to use Mike backend coding. Changed in CellReadPage.xaml and CellReadPage.xaml.cs

--- Game/Game/Views/Characters/CellReadPage.xaml.cs
+++ Game/Game/Views/Characters/CellReadPage.xaml.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace Game.Views.Characters
+namespace Game.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CellReadPage : ContentPage
