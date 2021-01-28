8027d3e91fe031d7277dc32f3edd542b72a416e1
Nitid Comvandee <ncomvandee@seattleu.edu>
2021-01-27 19:04:53 -0800

    Copied Mike's code to CellCreatePage. Changed in CellCreatePage.xaml.cs

--- Game/Game/Views/Characters/CellCreatePage.xaml.cs
+++ Game/Game/Views/Characters/CellCreatePage.xaml.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+
+using Game.Models;
+using Game.ViewModels;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -10,11 +14,27 @@ using Xamarin.Forms.Xaml;
 namespace Game.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class CellCreatePage : ContentPage
+    public partial class CellCreatePage : ContentPage      
     {
+
+        public GenericViewModel<CharacterModel> ViewModel = new GenericViewModel<CharacterModel>();
+
         public CellCreatePage()
         {
             InitializeComponent();
+
+            this.ViewModel.Data = new CharacterModel();
+
+            BindingContext = this.ViewModel;
+
+            this.ViewModel.Title = "Create";
+        }
+
+        public async void SaveButton_Clicked (object sender, EventArgs e)
+        {
+            MessagingCenter.Send(this, "Create", ViewModel.Data);
+
+            await Navigation.PopModalAsync();
         }
     }
 }
