ecf765321cd3c25fe7791134ebc2add4379d7997
Nitid Comvandee <ncomvandee@seattleu.edu>
2021-01-27 19:03:51 -0800

    Copied Mike's code to CellIndexPage. Chaged in CellIndePage.xaml and CellIndexPage.xaml.cs

--- Game/Game/Views/Characters/CellIndexPage.xaml.cs
+++ Game/Game/Views/Characters/CellIndexPage.xaml.cs
@@ -1,25 +1,93 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace Game.Views.Characters
+using Game.Models;
+using Game.ViewModels;
+using Game.Views;
+
+namespace Game.Views
 {
+    /// <summary>
+    /// Index Page
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0019:Use pattern matching", Justification = "<Pending>")]
+    [DesignTimeVisible(false)]
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CellIndexPage : ContentPage
     {
+        // The view model, used for data binding
+        readonly CellIndexViewModel ViewModel = CellIndexViewModel.Instance;
+
+        // Empty Constructor for UTs
+        public CellIndexPage(bool UnitTest) { }
+
+        /// <summary>
+        /// Constructor for Index Page
+        /// 
+        /// Get the ItemIndexView Model
+        /// </summary>
         public CellIndexPage()
         {
             InitializeComponent();
+
+            BindingContext = ViewModel;
         }
 
-        public async void CreateCell_Clicked(Object sender, EventArgs e)
+        /// <summary>
+        /// The row selected from the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
         {
-            await Navigation.PushModalAsync(new NavigationPage(new CellCreatePage()));
+            CharacterModel data = args.SelectedItem as CharacterModel;
+            if (data == null)
+            {
+                return;
+            }
+
+            // Open the Read Page
+            await Navigation.PushAsync(new CellReadPage(new GenericViewModel<CharacterModel>(data)));
+
+            // Manually deselect item.
+            ItemsListView.SelectedItem = null;
+        }
+
+        /// <summary>
+        /// Call to Add a new record
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void AddItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new ItemCreatePage()));
+        }
+
+        /// <summary>
+        /// Refresh the list on page appearing
+        /// </summary>
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            BindingContext = null;
+
+            // If no data, then set it for needing refresh
+            if (ViewModel.Dataset.Count == 0)
+            {
+                ViewModel.SetNeedsRefresh(true);
+            }
+
+            // If the needs Refresh flag is set update it
+            if (ViewModel.NeedsRefresh())
+            {
+                ViewModel.LoadDatasetCommand.Execute(null);
+            }
+
+            BindingContext = ViewModel;
         }
     }
 }
