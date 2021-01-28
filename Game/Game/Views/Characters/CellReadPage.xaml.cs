4ac25c81c540246373ff0b44c43854640724b681
Nitid Comvandee <ncomvandee@seattleu.edu>
2021-01-27 18:56:22 -0800

    Updated code to use Mike's Code in CellReadPage.xaml.cs

--- Game/Game/Views/Characters/CellReadPage.xaml.cs
+++ Game/Game/Views/Characters/CellReadPage.xaml.cs
@@ -1,20 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.ComponentModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using Game.ViewModels;
+using Game.Models;
+
 namespace Game.Views
 {
+    /// <summary>
+    /// The Read Page
+    /// </summary>
+    [DesignTimeVisible(false)]
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CellReadPage : ContentPage
     {
-        public CellReadPage()
+        // View Model for Item
+        public readonly GenericViewModel<CharacterModel> ViewModel;
+
+        // Empty Constructor for UTs
+        public CellReadPage(bool UnitTest) { }
+
+        /// <summary>
+        /// Constructor called with a view model
+        /// This is the primary way to open the page
+        /// The viewModel is the data that should be displayed
+        /// </summary>
+        /// <param name="viewModel"></param>
+        public CellReadPage(GenericViewModel<CharacterModel> data)
         {
             InitializeComponent();
+
+            BindingContext = this.ViewModel = data;
+        }
+
+        /// <summary>
+        /// Save calls to Update
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void Update_Clicked(object sender, EventArgs e)
+        {
+            //await Navigation.PushModalAsync(new NavigationPage(new ItemUpdatePage(ViewModel)));
+            //await Navigation.PopAsync();
+        }
+
+        /// <summary>
+        /// Calls for Delete
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void Delete_Clicked(object sender, EventArgs e)
+        {
+            //await Navigation.PushModalAsync(new NavigationPage(new ItemDeletePage(ViewModel)));
+            //await Navigation.PopAsync();
         }
     }
 }
