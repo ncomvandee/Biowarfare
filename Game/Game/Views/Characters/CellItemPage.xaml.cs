8c8d236f6bc18f09fe62bf44463ed4ec3fcb7ac0
Hung Huynh <huynhh1@seattleu.edu>
2021-01-29 21:04:38 -0800

    Add Cell Item Page to Project. Changed in CellItemPage.xaml CellItemPage.xaml.cs and Game.csproj

---
+++ Game/Game/Views/Characters/CellItemPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.ComponentModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Game.Models;
+using Game.ViewModels;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// Index Page
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0019:Use pattern matching", Justification = "<Pending>")]
+    [DesignTimeVisible(false)] 
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CellItemPage : ContentPage
+    {
+
+        // Empty Constructor for UTs
+        public CellItemPage(bool UnitTest) { }
+
+        /// <summary>
+        /// Constructor for Index Page
+        /// 
+        /// Get the ItemIndexView Model
+        /// </summary>
+        public CellItemPage()
+        {
+            InitializeComponent();
+
+         
+        }
+
+        /// <summary>
+        /// The row selected from the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            ItemModel data = args.SelectedItem as ItemModel;
+            if (data == null)
+            {
+                return;
+            }
+
+            // Open the Read Page
+            await Navigation.PushAsync(new ItemReadPage(new GenericViewModel<ItemModel>(data)));
+
+            // Manually deselect item.
+            ItemsListView.SelectedItem = null;
+        }
+
+        /// <summary>
+        /// Call to Add a new record
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void AddItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new ItemCreatePage()));
+        }
+
+    }
+}
