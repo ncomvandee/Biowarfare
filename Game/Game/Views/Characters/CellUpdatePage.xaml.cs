f61b13a206ea0d18f7760c3040dd41ac3f0b8b9b
Lindsjar37 <55968751+Lindsjar37@users.noreply.github.com>
2021-01-30 22:25:36 -0800

    Added cell Create page code to cell Update pages as starting point for Update page, used cell Create page methods so xaml wouldn't break on click. Changed CellUpdatePage.xaml, CellUpdatePage.Xaml.cs

--- Game/Game/Views/Characters/CellUpdatePage.xaml.cs
+++ Game/Game/Views/Characters/CellUpdatePage.xaml.cs
@@ -74,33 +74,93 @@ namespace Game.Views
         }
 
         /// <summary>
-        /// Catch the change to the Stepper for Range
+        /// Send Create message to ViewModel
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        public void Range_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        public async void SaveButton_Clicked(object sender, EventArgs e)
         {
-            RangeValue.Text = String.Format("{0}", e.NewValue);
+            MessagingCenter.Send(this, "Create", ViewModel.Data);
+
+            await Navigation.PopModalAsync();
         }
 
-        /// <summary>
-        /// Catch the change to the stepper for Value
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void Value_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        public async void CancelButton_Clicked(object sender, EventArgs e)
         {
-            ValueValue.Text = String.Format("{0}", e.NewValue);
+            await Navigation.PopModalAsync();
         }
 
-        /// <summary>
-        /// Catch the change to the stepper for Damage
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void Damage_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        public void ImageChanged(object sender, EventArgs e)
         {
-            DamageValue.Text = String.Format("{0}", e.NewValue);
+            var TypeSelected = CellTypePicker.SelectedItem.ToString();
+
+            switch (TypeSelected)
+            {
+                case "Basophil":
+                    CellImage.Source = "basophil_bg.png";
+                    break;
+
+                case "Eosinophil":
+                    CellImage.Source = "eosinophil_bg.png";
+                    break;
+
+                case "Macrophage":
+                    CellImage.Source = "macrophage_bg.png";
+                    break;
+
+                case "BCell":
+                    CellImage.Source = "";
+                    break;
+
+                case "KillerTCell":
+                    CellImage.Source = "";
+                    break;
+
+                case "NKCell":
+                    CellImage.Source = "";
+                    break;
+            }
         }
+
+
+
+
+
+
+
+
+
+
+
+
+        //    /// <summary>
+        //    /// Catch the change to the Stepper for Range
+        //    /// </summary>
+        //    /// <param name="sender"></param>
+        //    /// <param name="e"></param>
+        //    public void Range_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        //    {
+        //        RangeValue.Text = String.Format("{0}", e.NewValue);
+        //    }
+
+        //    /// <summary>
+        //    /// Catch the change to the stepper for Value
+        //    /// </summary>
+        //    /// <param name="sender"></param>
+        //    /// <param name="e"></param>
+        //    public void Value_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        //    {
+        //        ValueValue.Text = String.Format("{0}", e.NewValue);
+        //    }
+
+        //    /// <summary>
+        //    /// Catch the change to the stepper for Damage
+        //    /// </summary>
+        //    /// <param name="sender"></param>
+        //    /// <param name="e"></param>
+        //    public void Damage_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        //    {
+        //        DamageValue.Text = String.Format("{0}", e.NewValue);
+        //    }
     }
 }
