0670c13e9ad5bc83701e65f67eeb85fa91a4fb5f
Nitid Comvandee <ncomvandee@seattleu.edu>
2021-01-31 00:14:33 -0800

    Removed redundant update button handler in CellReadPage.xaml.cs

--- Game/Game/Views/Characters/CellReadPage.xaml.cs
+++ Game/Game/Views/Characters/CellReadPage.xaml.cs
@@ -35,17 +35,6 @@ namespace Game.Views
         }
 
         /// <summary>
-        /// Save calls to Update
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public async void Update_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushModalAsync(new NavigationPage(new CellUpdatePage(ViewModel)));
-            await Navigation.PopAsync();
-        }
-
-        /// <summary>
         /// Calls for Delete
         /// </summary>
         /// <param name="sender"></param>
