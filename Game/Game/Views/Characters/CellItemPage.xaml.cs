9cdfcc575ae8f96f7d22625b15a6c93dfd9897aa
Hung Huynh <huynhh1@seattleu.edu>
2021-01-31 14:43:04 -0800

    Add Save button in CellItemPage

--- Game/Game/Views/Characters/CellItemPage.xaml.cs
+++ Game/Game/Views/Characters/CellItemPage.xaml.cs
@@ -20,6 +20,7 @@ namespace Game.Views
         // View Model for cell
         public readonly GenericViewModel<CharacterModel> ViewModel;
 
+
         // Empty Constructor for UTs
         public CellItemPage(bool UnitTest) { }
 
@@ -35,37 +36,20 @@ namespace Game.Views
             BindingContext = this.ViewModel = data;
 
             this.ViewModel.Title = data.Data.Name + " Equipped Item" ;
+            
 
         }
 
         /// <summary>
-        /// The row selected from the list
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="args"></param>
-        public async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
-        {
-            ItemModel data = args.SelectedItem as ItemModel;
-            if (data == null)
-            {
-                return;
-            }
-
-            // Open the Read Page
-            await Navigation.PushAsync(new ItemReadPage(new GenericViewModel<ItemModel>(data)));
-
-            // Manually deselect item.
-            //ItemsListView.SelectedItem = null;
-        }
-
-        /// <summary>
-        /// Call to Add a new record
+        /// Send Save message to ViewModel
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        public async void AddItem_Clicked(object sender, EventArgs e)
+        public async void SaveButton_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushModalAsync(new NavigationPage(new ItemCreatePage()));
+            MessagingCenter.Send(this, "Update", ViewModel.Data);
+
+            await Navigation.PopModalAsync();
         }
 
     }
