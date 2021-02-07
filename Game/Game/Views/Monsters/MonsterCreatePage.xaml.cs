3c5a1d27e83009d576af39bb80cff434caefe86b
Nitid Comvandee <ncomvandee@seattleu.edu>
2021-02-06 23:58:51 -0800

    Refactored MonsterCreatePage.xaml.cs

--- Game/Game/Views/Monsters/MonsterCreatePage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterCreatePage.xaml.cs
@@ -17,13 +17,13 @@ namespace Game.Views
     public partial class MonsterCreatePage : ContentPage      
     {
 
-        // Maximum Cell Level
+        // Maximum Monster Level
         public int MaxLevel = 20;
 
-        // Minimum Cell Level
+        // Minimum Monster Level
         public int MinLevel = 1;
 
-        public GenericViewModel<CharacterModel> ViewModel = new GenericViewModel<CharacterModel>();
+        public GenericViewModel<MonsterModel> ViewModel = new GenericViewModel<MonsterModel>();
 
         /// <summary>
         /// Constructor
@@ -32,7 +32,7 @@ namespace Game.Views
         {
             InitializeComponent();
 
-            this.ViewModel.Data = new CharacterModel();
+            this.ViewModel.Data = new MonsterModel();
 
             BindingContext = this.ViewModel;
 
@@ -68,61 +68,50 @@ namespace Game.Views
         }
 
         /// <summary>
-        /// Open Cell Item Page
+        /// Changes Monster's thumbnail depends on selected MonsterType
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        public async void AddItem_Clicked (object sender, EventArgs e)
-        {
-            await Navigation.PushModalAsync(new NavigationPage(new CellItemPage(ViewModel)));
-            await Navigation.PopAsync();
-        }
+        //public void ImageChanged (object sender, EventArgs e)
+        //{
 
-        /// <summary>
-        /// Changes Cell's thumbnail depends on selected CellType
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void ImageChanged (object sender, EventArgs e)
-        {
+        //    CellTypePicker.BackgroundColor = Color.FromHex("#D8BFFF");
 
-            CellTypePicker.BackgroundColor = Color.FromHex("#D8BFFF");
+        //    // Get the string CellType from picker
+        //    var TypeSelected = CellTypePicker.SelectedItem.ToString();
 
-            // Get the string CellType from picker
-            var TypeSelected = CellTypePicker.SelectedItem.ToString();
+        //    switch (TypeSelected)
+        //    {
+        //        case "Basophil":
+        //            CellImage.Source = "basophil_bg.png";
+        //            ViewModel.Data.ImageURI = "basophil_bg.png";
+        //            break;
 
-            switch (TypeSelected)
-            {
-                case "Basophil":
-                    CellImage.Source = "basophil_bg.png";
-                    ViewModel.Data.ImageURI = "basophil_bg.png";
-                    break;
+        //        case "Eosinophil":
+        //            CellImage.Source = "eosinophil_bg.png";
+        //            ViewModel.Data.ImageURI = "eosinophil_bg.png";
+        //            break;
 
-                case "Eosinophil":
-                    CellImage.Source = "eosinophil_bg.png";
-                    ViewModel.Data.ImageURI = "eosinophil_bg.png";
-                    break;
+        //        case "Macrophage":
+        //            CellImage.Source = "macrophage_bg.png";
+        //            ViewModel.Data.ImageURI = "macrophage_bg.png";
+        //            break;
 
-                case "Macrophage":
-                    CellImage.Source = "macrophage_bg.png";
-                    ViewModel.Data.ImageURI = "macrophage_bg.png";
-                    break;
+        //        case "BCell":
+        //            CellImage.Source = "b_cell_bg.png";
+        //            ViewModel.Data.ImageURI = "b_cell_bg.png";
+        //            break;
 
-                case "BCell":
-                    CellImage.Source = "b_cell_bg.png";
-                    ViewModel.Data.ImageURI = "b_cell_bg.png";
-                    break;
+        //        case "KillerTCell":
+        //            CellImage.Source = "t_cell_bg.png";
+        //            ViewModel.Data.ImageURI = "t_cell_bg.png";
+        //            break;
 
-                case "KillerTCell":
-                    CellImage.Source = "t_cell_bg.png";
-                    ViewModel.Data.ImageURI = "t_cell_bg.png";
-                    break;
-
-                case "NKCell":
-                    CellImage.Source = "";
-                    break;
-            }
-        }
+        //        case "NKCell":
+        //            CellImage.Source = "";
+        //            break;
+        //    }
+        //}
 
         /// <summary>
         /// Set the value to change on Slider
@@ -145,67 +134,6 @@ namespace Game.Views
             {
                 SpeedStat.Text = String.Format("{0}", (int)e.NewValue);
             }
-        }
-
-        /// <summary>
-        /// Set the enable or disable for Leveling button
-        /// </summary>
-        public void SetEnableLevelButton()
-        {
-            LevelUpButton.IsEnabled = true;
-            if (ViewModel.Data.Level == MaxLevel)
-            {
-                LevelUpButton.IsEnabled = false;
-            }
-
-            LevelDownButton.IsEnabled = true;
-            if (ViewModel.Data.Level == MinLevel)
-            {
-                LevelDownButton.IsEnabled = false;
-            }
-
-        }
-
-        /// <summary>
-        /// Handling the Level Down button
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void LevelDownButtonClicked (object sender, EventArgs e)
-        {
-            ViewModel.Data.Level--;
-
-            if (ViewModel.Data.Level <= MinLevel)
-            {
-                ViewModel.Data.Level = MinLevel;
-            }
-
-            LevelEntry.Text = ViewModel.Data.Level.ToString();
-
-            // Call to set enable or disable the button
-            SetEnableLevelButton();
-
-        }
-
-        /// <summary>
-        /// Handling the Level Up button
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void LevelUpButtonClicked (object sender, EventArgs e)
-        {
-
-            ViewModel.Data.Level++;
-
-            if (ViewModel.Data.Level >= MaxLevel)
-            {
-                ViewModel.Data.Level = MaxLevel;
-            }
-
-            LevelEntry.Text = ViewModel.Data.Level.ToString();
-
-            // Call to set enable or disable the button
-            SetEnableLevelButton();
         }
 
         /// <summary>
