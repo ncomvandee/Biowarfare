0e083a6602703a871efae1c95b82d42659bdbd4b
Nitid Comvandee <ncomvandee@seattleu.edu>
2021-02-07 23:12:24 -0800

    Updated MonsterCellReadPage for using MonsterModel. Changed in MonsterCellReadPage.xaml and MonsterCellReadPage.xaml.cs

--- Game/Game/Views/Monsters/MonsterCellReadPage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterCellReadPage.xaml.cs
@@ -50,7 +50,7 @@ namespace Game.Views
 		/// </summary>
 		/// <param name="sender"></param>
 		/// <param name="e"></param>
-		public async void CellEditButton_Clicked(object sender, EventArgs e)
+		public async void MonsterCellEditButton_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushModalAsync(new NavigationPage(new MonsterCellUpdatePage(ViewModel)));
 
