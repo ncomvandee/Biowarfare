89d9bd44a259e2153e40849d4c7597a199798705
Lindsjar37 <55968751+Lindsjar37@users.noreply.github.com>
2021-02-14 12:41:15 -0800

    Changed picker color to match in CellUpdatePage.xaml

--- Game/Game/Views/Characters/CellUpdatePage.xaml.cs
+++ Game/Game/Views/Characters/CellUpdatePage.xaml.cs
@@ -100,7 +100,7 @@ namespace Game.Views
         /// <param name="e"></param>
         public void ImageChanged(object sender, EventArgs e)
         {
-            CellTypePicker.BackgroundColor = Color.FromHex("#D8BFFF");
+            CellTypePicker.BackgroundColor = Color.FromHex("#3CAEA3");
 
             // Get the string CellType from picker
             var TypeSelected = CellTypePicker.SelectedItem.ToString();
