34752226c88e7a486d3a07e68c8305e28b61fa94
Lindsjar37 <55968751+Lindsjar37@users.noreply.github.com>
2021-02-15 00:07:29 -0800

    Added UniqueItemIndexPage.xaml, UniqueItemIndexPage.xaml.cs, Populated with same code as Consumable Item Index Page.

--- Game/Game/Views/Items/UniqueItemIndexPage.xaml.cs
+++ Game/Game/Views/Items/UniqueItemIndexPage.xaml.cs
@@ -9,12 +9,80 @@ using Xamarin.Forms.Xaml;
 
 namespace Game.Views.Items
 {
+    /// <summary>
+    /// Index Page
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0019:Use pattern matching", Justification = "<Pending>")]
+    [DesignTimeVisible(false)]
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class UniqueItemIndexPage : ContentPage
     {
+
+        // The view model, used for data binding
+        readonly ConsumableItemIndexViewModel ViewModel = ConsumableItemIndexViewModel.Instance;
+
+        // Empty Constructor for UTs
+        public ConsumableItemIndexPage(bool UnitTest) { }
+
         public UniqueItemIndexPage()
         {
             InitializeComponent();
+
+            BindingContext = ViewModel;
+        }
+
+        /// <summary>
+        /// The row selected from the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            ItemModel data = args.SelectedItem as ItemModel;
+            if (data == null)
+            {
+                return;
+            }
+
+            // Open the Read Page
+            await Navigation.PushAsync(new ItemReadPage(new GenericViewModel<ItemModel>(data)));
+
+            // Manually deselect item.
+            ItemsListView.SelectedItem = null;
+        }
+
+        /// <summary>
+        /// Call to Add a new record
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void AddItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new ItemCreatePage()));
+        }
+
+        /// <summary>
+        /// Refresh the list on page appearing
+        /// </summary>
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            BindingContext = null;
+
+            // If no data, then set it for needing refresh
+            if (ViewModel.Dataset.Count == 0)
+            {
+                ViewModel.SetNeedsRefresh(true);
+            }
+
+            // If the needs Refresh flag is set update it
+            if (ViewModel.NeedsRefresh())
+            {
+                ViewModel.LoadDatasetCommand.Execute(null);
+            }
+
+            BindingContext = ViewModel;
         }
     }
 }
