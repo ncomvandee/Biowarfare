5f2aaa9ccc7c69c03e1949c5ec3a774a5ce0c8e8
Nitid Comvandee <ncomvandee@seattleu.edu>
2021-02-22 12:52:09 -0800

    Added ConsumableItemIndexPageTests.cs to UnitTest

---
+++ UnitTests/ViewsItems/ConsumableItemIndexPageTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+
+using Game;
+using Game.Views;
+using Game.Models;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Mocks;
+using Game.ViewModels;
+using System.Threading.Tasks;
+using Game.Views.Items;
+
+namespace UnitTests.ViewsItems
+{
+    [TestFixture]
+    public class ConsumableItemIndexPageTests : ConsumableItemIndexPage
+    {
+        App app;
+        ConsumableItemIndexPage page;
+
+        public ConsumableItemIndexPageTests() : base(true) { }
+        
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            page = new ConsumableItemIndexPage();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Application.Current = null;
+        }
+
+        [Test]
+        public void ConsumableItemIndexPage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = page;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        
+    }
+}
