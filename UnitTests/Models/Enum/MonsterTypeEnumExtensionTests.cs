4877a48bbae210f8b960429fda604d6737951bc1
Nitid Comvandee <ncomvandee@seattleu.edu>
2021-02-22 02:59:40 -0800

    Added MonsterTypeEnumExtensionTests.cs to UnitTest

---
+++ UnitTests/Models/Enum/MonsterTypeEnumExtensionTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+using Game.Models;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class MonsterTypeEnumExtensionTests
+    {
+        [Test]
+        public void CharacterJobEnumExtensionsTests_Unknown_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = CellTypeEnum.Unknown.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Player", result);
+        }
+
+        
+    }
+}
