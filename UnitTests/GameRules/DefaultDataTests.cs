856204b888001bb7d7b70fea9cbe7b4fb206d1c6
Hung Huynh <huynhh1@seattleu.edu>
2021-02-21 19:40:10 -0800

    Change assert equal number for item to 27 in DefaultDataTest

--- UnitTests/GameRules/DefaultDataTests.cs
+++ UnitTests/GameRules/DefaultDataTests.cs
@@ -21,7 +21,7 @@ namespace UnitTests.GameRules
             // Reset
 
             // Assert
-            Assert.AreEqual(26, result.Count());
+            Assert.AreEqual(27, result.Count());
         }
 
         [Test]
