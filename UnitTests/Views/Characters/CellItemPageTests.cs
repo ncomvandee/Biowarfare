8e9c4d828aea4ae978077b8187191b3a374319b8
Hung Huynh <huynhh1@seattleu.edu>
2021-02-22 13:23:02 -0800

    Add first constructor test for CellItemPageTest

--- UnitTests/Views/Characters/CellItemPageTests.cs
+++ UnitTests/Views/Characters/CellItemPageTests.cs
@@ -1,10 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NUnit.Framework;
 
-namespace UnitTests.Views.Characters
+using Game;
+using Game.Views;
+using Game.ViewModels;
+using Game.Models;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Mocks;
+using System.Linq;
+
+namespace UnitTests.Views
 {
-    class CellItemPageTests
+    [TestFixture]
+    public class CellItemPageTests : CellItemPage
     {
+        App app;
+        CellItemPage page;
+
+        public CellItemPageTests() : base(true) { }
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            page = new CellItemPage(new GenericViewModel<CharacterModel>(new CharacterModel()));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Application.Current = null;
+        }
+
+        [Test]
+        public void CellItemPage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = page;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
     }
 }
