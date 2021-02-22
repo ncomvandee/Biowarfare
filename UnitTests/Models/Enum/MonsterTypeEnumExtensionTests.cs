e64d80a7059c905a014be981bab0758127a432ed
Nitid Comvandee <ncomvandee@seattleu.edu>
2021-02-22 03:07:55 -0800

    Added tests for ToMessage in MonsterTypeEnumExtensionTests.cs

--- UnitTests/Models/Enum/MonsterTypeEnumExtensionTests.cs
+++ UnitTests/Models/Enum/MonsterTypeEnumExtensionTests.cs
@@ -8,19 +8,88 @@ namespace UnitTests.Models
     public class MonsterTypeEnumExtensionTests
     {
         [Test]
-        public void CharacterJobEnumExtensionsTests_Unknown_Default_Should_Pass()
+        public void MonsterTypeEnumExtensionsTests_Unknown_Default_Should_Pass()
         {
             // Arrange
 
             // Act
-            var result = CellTypeEnum.Unknown.ToMessage();
+            var result = MonsterTypeEnum.Unknown.ToMessage();
 
             // Reset
 
             // Assert
-            Assert.AreEqual("Player", result);
+            Assert.AreEqual("Monster", result);
         }
 
-        
+        [Test]
+        public void MonsterTypeEnumExtensionTests_ToMessage_Spore_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = MonsterTypeEnum.Spore.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Spore", result);
+        }
+
+        [Test]
+        public void MonsterTypeEnumExtensionTests_ToMessage_Bacteria_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = MonsterTypeEnum.Bacteria.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Bacteria", result);
+        }
+
+        [Test]
+        public void MonsterTypeEnumExtensionTests_ToMessage_Parasite_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = MonsterTypeEnum.Parasite.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Parasite", result);
+        }
+
+        [Test]
+        public void MonsterTypeEnumExtensionTests_ToMessage_Virus_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = MonsterTypeEnum.Virus.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Virus", result);
+        }
+
+        [Test]
+        public void MonsterTypeEnumExtensionTests_ToMessage_Cancer_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = MonsterTypeEnum.Cancer.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("BIG BOSS CANCER", result);
+        }
+
     }
 }
