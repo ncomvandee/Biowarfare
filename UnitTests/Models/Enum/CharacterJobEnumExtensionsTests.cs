6dc5f5ac7a54966c608f342432f02e727b2cfb2d
Nitid Comvandee <ncomvandee@seattleu.edu>
2021-02-22 02:42:34 -0800

    Added tests for ToMessage in CharacterJobEnumExtensionsTests.cs

--- UnitTests/Models/Enum/CharacterJobEnumExtensionsTests.cs
+++ UnitTests/Models/Enum/CharacterJobEnumExtensionsTests.cs
@@ -21,6 +21,62 @@ namespace UnitTests.Models
             Assert.AreEqual("Player", result);
         }
 
+        [Test]
+        public void CellTypeEnumExtensionTests_ToMessage_Killer_T_Cell_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = CellTypeEnum.KillerTCell.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Killer-T-Cell", result);
+        }
+
+        [Test]
+        public void CellTypeEnumExtensionTests_ToMessage_NK_Cell_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = CellTypeEnum.NKCell.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("NK-Cell", result);
+        }
+
+        [Test]
+        public void CellTypeEnumExtensionTests_ToMessage_Eosinophil_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = CellTypeEnum.Eosinophil.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Eosinophil", result);
+        }
+
+        [Test]
+        public void CellTypeEnumExtensionTests_ToMessage_Basophil_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = CellTypeEnum.Basophil.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Basophil", result);
+        }
+
         //[Test]
         //public void CharacterJobEnumExtensionsTests_Fighter_Default_Should_Pass()
         //{
