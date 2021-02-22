895a8d545054d464f0470c48d4b9e733bdf3fe20
Lindsjar37 <55968751+Lindsjar37@users.noreply.github.com>
2021-02-21 21:35:08 -0800

    Added method comments to itemIndexNavigation.xaml.cs

--- Game/Game/Views/Items/ItemIndexNavigation.xaml.cs
+++ Game/Game/Views/Items/ItemIndexNavigation.xaml.cs
@@ -9,9 +9,15 @@ using Xamarin.Forms.Xaml;
 
 namespace Game.Views.Items
 {
+    /// <summary>
+    /// Tabbed page for easy item index navigation between item types. 
+    /// </summary>
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ItemIndexNavigation : TabbedPage
     {
+        /// <summary>
+        /// Constructor 
+        /// </summary>
         public ItemIndexNavigation()
         {
             InitializeComponent();
