a72ae2b8ad00eee2edd58428556b114b362961db
Hung Huynh <huynhh1@seattleu.edu>
2021-02-21 18:08:16 -0800

    Fix Roll Dice bug in DiceHelper.cs

--- Game/Game/Helpers/DiceHelper.cs
+++ Game/Game/Helpers/DiceHelper.cs
@@ -44,19 +44,20 @@ namespace Game.Helpers
         /// <returns></returns>
         public static int RollDice(int rolls, int dice)
         {
-            if (ForceRollsToNotRandom)
-            {
-                return rolls * _ForcedRandomValue;
-            }
-
             if (rolls < 1)
             {
                 return 0;
             }
 
+
             if (dice < 1)
             {
                 return 0;
+            }
+
+            if (ForceRollsToNotRandom)
+            {
+                return rolls * _ForcedRandomValue;
             }
 
             var myReturn = 0;
