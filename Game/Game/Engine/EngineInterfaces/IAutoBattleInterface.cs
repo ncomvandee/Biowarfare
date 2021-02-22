6f13caef56b577c9c9ccc84af609cf73d5e73bd4
Lindsjar37 <55968751+Lindsjar37@users.noreply.github.com>
2021-02-21 20:11:24 -0800

    Added method comment to IAutoBattleInterface.cs

--- Game/Game/Engine/EngineInterfaces/IAutoBattleInterface.cs
+++ Game/Game/Engine/EngineInterfaces/IAutoBattleInterface.cs
@@ -2,6 +2,9 @@
 
 namespace Game.Engine.EngineInterfaces
 {
+    /// <summary>
+    /// Holds data structures for Auto Battle Engine
+    /// </summary>
     public interface IAutoBattleInterface
     {
         IBattleEngineInterface Battle { get;}
