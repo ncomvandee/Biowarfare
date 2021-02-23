483cfc4a2a82922b9da79568cfa4c0ac7d5a11ab
Nitid Comvandee <ncomvandee@seattleu.edu>
2021-02-22 17:19:10 -0800

    Added CellCreatePage_OnSliderChanged_AttackSlider_Valid_Default_Should_Pass in CellCreatePageTests.cs

--- UnitTests/Views/Characters/CellCreatePageTests.cs
+++ UnitTests/Views/Characters/CellCreatePageTests.cs
@@ -183,6 +183,26 @@ namespace UnitTests.Views
             Assert.IsTrue(true);
         }
 
+        [Test]
+        public void CellCreatePage_OnSliderChanged_AttackSlider_Valid_Default_Should_Pass()
+        {
+            // Arrange
+            page = new CellCreatePage();
+
+            var AttackSlider = page.FindByName<Slider>("AttackSlider");
+
+            double OldVal = 2.0;
+            double NewVal = 5.0;
+
+            var arg = new ValueChangedEventArgs(OldVal, NewVal);
+
+            // Act
+            page.OnSliderChanged(AttackSlider, arg);
+
+            // Assert
+            Assert.IsTrue(true);
+        }
+
         //[Test]
         //public void CharacterCreatePage_Attack_OnStepperAttackChanged_Default_Should_Pass()
         //{
