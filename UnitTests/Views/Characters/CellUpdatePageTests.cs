6d4ca9d971f458b09e5d19181eb9ef5e82dfd5d7
Hung Huynh <huynhh1@seattleu.edu>
2021-02-22 19:13:21 -0800

    Add test for attack slider in CellUpdatePageTests

--- UnitTests/Views/Characters/CellUpdatePageTests.cs
+++ UnitTests/Views/Characters/CellUpdatePageTests.cs
@@ -141,6 +141,45 @@ namespace UnitTests.Views
             Assert.AreEqual(MinLevel, result); // 
         }
 
+        [Test]
+        public void CellUpdatePage_LevelUpButtonClicked_Character_MaxLevel_Should_Equal_To_MaxLevel()
+        {
+            // Arrange
+            page.ViewModel.Data = new CharacterModel()
+            {
+                Id = "test",
+                Level = MaxLevel
+            };
+            // Act
+            page.LevelUpButtonClicked(null, null);
+            var result = page.ViewModel.Data.Level;
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(MaxLevel, result); // 
+        }
+
+        [Test]
+        public void CellUpdatePage_OnSliderChanged_AttackSlider_Should_Pass()
+        {
+            // Arrange
+            page.ViewModel.Data = new CharacterModel()
+            {
+                Id = "test",
+                Level = MaxLevel
+            };
+
+            
+            var control = (Slider)page.FindByName("AttackSlider");
+            ValueChangedEventArgs e = new ValueChangedEventArgs(page.ViewModel.Data.Attack,1);
+            // Act
+            page.OnSliderChanged(control,e);
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // 
+        }
         //[Test]
         //public void CharacterUpdatePage_Attack_OnStepperValueChanged_Default_Should_Pass()
         //{
