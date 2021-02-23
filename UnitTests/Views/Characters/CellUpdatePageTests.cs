868a82545e9edab7d66b66e5ca01a58bfc2f22a9
Hung Huynh <huynhh1@seattleu.edu>
2021-02-22 19:32:39 -0800

    Add test for speedslider in CellUpdatePagetests

--- UnitTests/Views/Characters/CellUpdatePageTests.cs
+++ UnitTests/Views/Characters/CellUpdatePageTests.cs
@@ -161,7 +161,7 @@ namespace UnitTests.Views
         }
 
         [Test]
-        public void CellUpdatePage_OnSliderChanged_AttackSlider_Should_Equal_to_1()
+        public void CellUpdatePage_OnSliderChanged_AttackSlider_Should_Equal_To_1()
         {
             // Arrange
             page.ViewModel.Data = new CharacterModel()
@@ -183,7 +183,7 @@ namespace UnitTests.Views
         }
 
         [Test]
-        public void CellUpdatePage_OnSliderChanged_DefenseSlider_Should_Pass()
+        public void CellUpdatePage_OnSliderChanged_DefenseSlider__Should_Equal_To_1()
         {
             // Arrange
             page.ViewModel.Data = new CharacterModel()
@@ -191,7 +191,7 @@ namespace UnitTests.Views
                 Id = "test",
                 Level = MaxLevel
             };
-
+            var statText = (Label)page.FindByName("DefenseStat");
 
             var control = (Slider)page.FindByName("DefenseSlider");
             ValueChangedEventArgs e = new ValueChangedEventArgs(page.ViewModel.Data.Attack, 1);
@@ -200,8 +200,31 @@ namespace UnitTests.Views
             // Reset
 
             // Assert
-            Assert.IsTrue(true); // 
+            Assert.AreEqual("1", statText.Text); // 
         }
+
+        [Test]
+        public void CellUpdatePage_OnSliderChanged_SpeedSlider__Should_Equal_To_1()
+        {
+            // Arrange
+            page.ViewModel.Data = new CharacterModel()
+            {
+                Id = "test",
+                Level = MaxLevel
+            };
+            var statText = (Label)page.FindByName("SpeedStat");
+
+            var control = (Slider)page.FindByName("SpeedSlider");
+            ValueChangedEventArgs e = new ValueChangedEventArgs(page.ViewModel.Data.Attack, 1);
+            // Act
+            page.OnSliderChanged(control, e);
+            // Reset
+
+            // Assert
+            Assert.AreEqual("1", statText.Text); // 
+        }
+
+
         //[Test]
         //public void CharacterUpdatePage_Attack_OnStepperValueChanged_Default_Should_Pass()
         //{
