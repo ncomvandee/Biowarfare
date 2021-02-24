76fa38cfc1674826160847ae5865818c069aa206
Lindsjar37 <55968751+Lindsjar37@users.noreply.github.com>
2021-02-23 22:44:34 -0800

    Added code to the constructor of the home page to change the background every 2 seconds. Changed HomePage.xaml.cs

--- Game/Game/Views/Home/HomePage.xaml.cs
+++ Game/Game/Views/Home/HomePage.xaml.cs
@@ -18,6 +18,30 @@ namespace Game.Views
 			InitializeComponent ();
 
 			NavigationPage.SetHasNavigationBar(this, false);
+
+			// Bool to set the background style based on timer 
+			bool originalStyle = true;
+
+				// Device timer will change the background color every 2 seconds from purple to red 
+				Device.StartTimer(TimeSpan.FromSeconds(2), () =>
+					{
+						if (originalStyle)
+						{
+							// Change the background color to purple 
+							Resources["baseStyle"] = Resources["PurpleHomePageContentStyle"];
+							originalStyle = false;
+						}
+						else
+						{
+							// Change the background color to red 
+							Resources["baseStyle"] = Resources["RedHomePageContentStyle"];
+							originalStyle = true;
+						}
+
+						// Repeat again indefinately 
+						return true; 
+				});
+	
 		}
 
 		/// <summary>
