d40d787c09b53248e656aac156c9545e809730c0
Mike Koenig <koenigm@seattleu.edu>
2021-02-26 13:36:37 -0800

    Changed to use GameEngine instead of Koenig in AutoBattlePage.xaml.cs

--- Game/Game/Views/Battle/AutoBattlePage.xaml.cs
+++ Game/Game/Views/Battle/AutoBattlePage.xaml.cs
@@ -47,7 +47,7 @@ namespace Game.Views
 			var CharacterPlayer = new PlayerInfoModel(Character);
 
 			// Turn on the Koenig version for now...
-			BattleEngineViewModel.Instance.SetBattleEngineToKoenig();
+			//BattleEngineViewModel.Instance.SetBattleEngineToKoenig();
 
 			BattleEngineViewModel.Instance.Engine.EngineSettings.CharacterList.Add(CharacterPlayer);
 
