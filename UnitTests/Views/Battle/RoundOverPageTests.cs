962dd4b861c5385ecb33444d0f9e45711a2082b1
Hung Huynh <huynhh1@seattleu.edu>
2021-03-08 23:10:34 -0800

    Add scorebutton click in RoundOverPageTest

--- UnitTests/Views/Battle/RoundOverPageTests.cs
+++ UnitTests/Views/Battle/RoundOverPageTests.cs
@@ -300,5 +300,22 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void RoundOverPage_ScoreButton_Clicked_Clicked_Should_Pass()
+        {
+            // Arrange
+
+
+
+
+            // Act
+            page.ScoreButton_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
