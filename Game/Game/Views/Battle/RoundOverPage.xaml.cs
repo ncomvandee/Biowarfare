7ee0a18a7c08b1cc35a6a5baa6aba3c472e8f46a
Nitid Comvandee <ncomvandee@seattleu.edu>
2021-03-13 00:51:44 -0800

    Added items frame to show items cell equipped. Change in RoundOverPage.xaml and RoundOverPage.xaml.cs

--- Game/Game/Views/Battle/RoundOverPage.xaml.cs
+++ Game/Game/Views/Battle/RoundOverPage.xaml.cs
@@ -295,6 +295,90 @@ namespace Game.Views
         }
 
         /// <summary>
+        /// Displays items Cell equipped as image
+        /// </summary>
+        public void AddItemToDisplay(PlayerInfoModel CellData)
+        {
+            var FlexList = ItemBox.Children.ToList();
+
+            foreach (var data in FlexList)
+            {
+                ItemBox.Children.Remove(data);
+            }
+
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.Head, CellData));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.Necklass, CellData));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.PrimaryHand, CellData));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.OffHand, CellData));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.RightFinger, CellData));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.LeftFinger, CellData));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.Feet, CellData));
+
+        }
+
+        /// <summary>
+        /// Look up items to display
+        /// </summary>
+        /// <param name="location"></param>
+        /// <returns></returns>
+        public StackLayout GetItemToDisplay(ItemLocationEnum location, PlayerInfoModel data)
+        {
+            var ItemData = data.GetItemByLocation(location);
+
+            // If item is not equipped, will not render anything
+            if (ItemData == null)
+            {
+
+                return new StackLayout { };
+
+            }
+
+            // Item image as ImageButton for pop-up detail view
+            var ItemButton = new ImageButton
+            {
+                Source = ItemData.ImageURI,
+                WidthRequest = 60,
+                HeightRequest = 60,
+                BackgroundColor = Color.White,
+                CornerRadius = 50,
+                Padding = 5,
+            };
+
+            // Label for each item
+            var ItemLabel = new Label
+            {
+                Text = ItemData.Attribute.ToString() + " + " + ItemData.Value.ToString(),
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalTextAlignment = TextAlignment.Center,
+                HorizontalTextAlignment = TextAlignment.Center,
+                LineBreakMode = LineBreakMode.HeadTruncation
+
+            };
+
+            // Frame for label, so that the text will wrap if text is long
+            var ItemFrame = new Frame
+            {
+                BackgroundColor = Color.Transparent,
+                BorderColor = Color.Transparent,
+                WidthRequest = 100,
+
+                Content = ItemLabel,
+            };
+
+            // Put ImageButton and label in to same stack
+            var ItemStack = new StackLayout
+            {
+                Padding = 3,
+                HorizontalOptions = LayoutOptions.Center,
+
+                Children = { ItemButton, ItemFrame },
+            };
+
+            // Render particuler stack
+            return ItemStack;
+        }
+
+        /// <summary>
         /// Show the Popup for the present character information
         /// </summary>
         /// <param name="data"></param>
@@ -311,6 +395,8 @@ namespace Game.Views
             Speed.Text = data.GetSpeedTotal.ToString();
             Health.Text = data.GetCurrentHealthTotal.ToString();
             Experience.Text = data.ExperienceTotal.ToString();
+
+            AddItemToDisplay(data);
             return true;
         }
 
@@ -406,6 +492,30 @@ namespace Game.Views
         public async void PickItem_Clicked(ItemModel data)
         {
             await Navigation.PushModalAsync(new PickItemsPage(new GenericViewModel < ItemModel >(data)));
+        }
+
+        /// <summary>
+        /// Flip feature, toggle betweem item list and attribute
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void ItemAttributeToggle_Clicked(object sender, EventArgs e)
+        {
+            if (ItemsFrame.IsVisible)
+            {
+                ItemsFrame.IsVisible = false;
+                AttributeFrame.IsVisible = true;
+                ItemAttributeToggleButton.Text = "Attributes";
+                return;
+            }
+
+            if (AttributeFrame.IsVisible)
+            {
+                AttributeFrame.IsVisible = false;
+                ItemsFrame.IsVisible = true;
+                ItemAttributeToggleButton.Text = "Items";
+                return;
+            }
         }
     }
 }
