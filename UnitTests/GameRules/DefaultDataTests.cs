3c226b4140c1da73a91aee911cdd33cf982eca63
Hung Huynh <huynhh1@seattleu.edu>
2021-03-15 09:53:59 -0700

    Change defualt data to 7 in DeafultDataTest

--- UnitTests/GameRules/DefaultDataTests.cs
+++ UnitTests/GameRules/DefaultDataTests.cs
@@ -21,7 +21,7 @@ namespace UnitTests.GameRules
             // Reset
 
             // Assert
-            Assert.AreEqual(27, result.Count());
+            Assert.AreEqual(7, result.Count());
         }
 
         [Test]
