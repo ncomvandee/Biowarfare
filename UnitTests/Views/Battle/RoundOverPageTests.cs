46dd02b09aa8b15748374e6400a515fef215bd8f
Lindsjar37 <55968751+Lindsjar37@users.noreply.github.com>
2021-03-16 13:02:42 -0700

    Added RoundOverPage_UnequippedItem__Necklass_Should_Pass() to RoundOverPageTests.cs

--- UnitTests/Views/Battle/RoundOverPageTests.cs
+++ UnitTests/Views/Battle/RoundOverPageTests.cs
@@ -430,5 +430,22 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void RoundOverPage_UnequippedItem__Necklass_Should_Pass()
+        {
+            // Arrange
+            var data = new PlayerInfoModel(new CharacterModel { Name = "test" });
+            var dataTest = new ItemModel { Name = "test", Location = ItemLocationEnum.Necklass };
+            data.AddItem(dataTest.Location, dataTest.Guid);
+            //var item = ItemIndexViewModel.Instance.GetDefaultItem(ItemLocationEnum.PrimaryHand);
+            // Act
+
+            page.UnequippedItem(data, dataTest.Location);
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
