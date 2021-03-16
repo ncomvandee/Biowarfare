a40b9b50bc781652a879b72b9c1a2da4d6250329
Lindsjar37 <55968751+Lindsjar37@users.noreply.github.com>
2021-03-16 12:57:45 -0700

    Added RoundOverPage_UnequippedItem__PrimaryHand_Should_Pass() to RoundOverPageTests.cs

--- UnitTests/Views/Battle/RoundOverPageTests.cs
+++ UnitTests/Views/Battle/RoundOverPageTests.cs
@@ -395,5 +395,23 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        
+        [Test]
+        public void RoundOverPage_UnequippedItem__PrimaryHand_Should_Pass()
+        {
+            // Arrange
+            var data = new PlayerInfoModel(new CharacterModel { Name = "test" }); 
+            var dataTest = new ItemModel { Name = "test", Location = ItemLocationEnum.PrimaryHand };
+            data.AddItem(dataTest.Location, dataTest.Guid); 
+            //var item = ItemIndexViewModel.Instance.GetDefaultItem(ItemLocationEnum.PrimaryHand);
+            // Act
+
+            page.UnequippedItem(data, dataTest.Location);
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
